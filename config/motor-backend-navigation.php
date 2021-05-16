9fbc34a2870b1d98ccf856c997f7a47555073f9b
Reza Esmaili <bitbucket@dfox.info>
2021-05-16 22:44:44 +0200

    Change navigation routes

--- config/motor-backend-navigation.php
+++ config/motor-backend-navigation.php
@@ -4,15 +4,15 @@ return [
     'items' => [
         5 => [
             'slug'        => 'revision',
-            'name'        => 'motor-revision::backend/global.revision',
-            'icon'        => 'fa fa-home',
+            'name'        => 'motor-revision.global.revision',
+            'icon'        => 'paper-plane',
             'route'       => null,
             'roles'       => ['SuperAdmin'],
             'permissions' => ['revision.read'],
             'items'       => [
                 100 => [
                     'slug'        => 'tickets',
-                    'name'        => 'motor-revision::backend/tickets.tickets',
+                    'name'        => 'motor-revision.tickets.tickets',
                     'icon'        => 'fa fa-plus',
                     'route'       => 'backend.tickets.index',
                     'roles'       => ['SuperAdmin'],
@@ -20,7 +20,7 @@ return [
                 ],
                 200 => [ // <-- !!! replace 531 with your own sort position !!!
                          'slug'        => 'airports',
-                         'name'        => 'motor-revision::backend/airports.airports',
+                         'name'        => 'motor-revision.airports.airports',
                          'icon'        => 'fa fa-plus',
                          'route'       => 'backend.airports.index',
                          'roles'       => ['SuperAdmin'],
@@ -28,7 +28,7 @@ return [
                 ],
                 300 => [ // <-- !!! replace 341 with your own sort position !!!
                          'slug'        => 'travelers',
-                         'name'        => 'motor-revision::backend/travelers.travelers',
+                         'name'        => 'motor-revision.travelers.travelers',
                          'icon'        => 'fa fa-plus',
                          'route'       => 'backend.travelers.index',
                          'roles'       => ['SuperAdmin'],
@@ -36,7 +36,7 @@ return [
                 ],
                 400 => [ // <-- !!! replace 165 with your own sort position !!!
                          'slug'        => 'shuttles',
-                         'name'        => 'motor-revision::backend/shuttles.shuttles',
+                         'name'        => 'motor-revision.shuttles.shuttles',
                          'icon'        => 'fa fa-plus',
                          'route'       => 'backend.shuttles.index',
                          'roles'       => ['SuperAdmin'],
@@ -44,7 +44,7 @@ return [
                 ],
                 500 => [ // <-- !!! replace 187 with your own sort position !!!
                          'slug'        => 'rides',
-                         'name'        => 'motor-revision::backend/rides.rides',
+                         'name'        => 'motor-revision.rides.rides',
                          'icon'        => 'fa fa-plus',
                          'route'       => 'backend.rides.index',
                          'roles'       => ['SuperAdmin'],
@@ -52,7 +52,7 @@ return [
                 ],
                 600 => [ // <-- !!! replace 371 with your own sort position !!!
                          'slug'        => 'sponsors',
-                         'name'        => 'motor-revision::backend/sponsors.sponsors',
+                         'name'        => 'motor-revision.sponsors.sponsors',
                          'icon'        => 'fa fa-plus',
                          'route'       => 'backend.sponsors.index',
                          'roles'       => ['SuperAdmin'],
@@ -60,7 +60,7 @@ return [
                 ],
                 700 => [ // <-- !!! replace 290 with your own sort position !!!
                          'slug'        => 'hotels',
-                         'name'        => 'motor-revision::backend/hotels.hotels',
+                         'name'        => 'motor-revision.hotels.hotels',
                          'icon'        => 'fa fa-plus',
                          'route'       => 'backend.hotels.index',
                          'roles'       => ['SuperAdmin'],
