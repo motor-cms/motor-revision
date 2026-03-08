e1ad35c7c86bf9ef5cd591ef6bb84d7cd7418adb
Reza Esmaili <github@dfox.info>
2026-03-08 10:55:59 +0100

    Modernize routes: array callable syntax, remove namespace declarations

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,23 @@
 <?php
+
+use Motor\Revision\Http\Controllers\Api\TicketsController;
+use Motor\Revision\Http\Controllers\Api\AirportsController;
+use Motor\Revision\Http\Controllers\Api\TravelersController;
+use Motor\Revision\Http\Controllers\Api\ShuttlesController;
+use Motor\Revision\Http\Controllers\Api\RidesController;
+use Motor\Revision\Http\Controllers\Api\SponsorsController;
+use Motor\Revision\Http\Controllers\Api\HotelsController;
+
 Route::group([
     'middleware' => ['auth:api', 'bindings', 'permission'],
-    'namespace'  => 'Motor\Revision\Http\Controllers\Api',
     'prefix'     => 'api',
     'as'         => 'api.',
 ], static function () {
-    Route::apiResource('tickets', 'TicketsController');
-    Route::apiResource('airports', 'AirportsController');
-    Route::apiResource('travelers', 'TravelersController');
-    Route::apiResource('shuttles', 'ShuttlesController');
-    Route::apiResource('rides', 'RidesController');
-    Route::apiResource('sponsors', 'SponsorsController');
-    Route::apiResource('hotels', 'HotelsController');
+    Route::apiResource('tickets', TicketsController::class);
+    Route::apiResource('airports', AirportsController::class);
+    Route::apiResource('travelers', TravelersController::class);
+    Route::apiResource('shuttles', ShuttlesController::class);
+    Route::apiResource('rides', RidesController::class);
+    Route::apiResource('sponsors', SponsorsController::class);
+    Route::apiResource('hotels', HotelsController::class);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,47 @@
 <?php
+
+use Motor\Revision\Http\Controllers\Backend\TicketsController;
+use Motor\Revision\Http\Controllers\Backend\AirportsController;
+use Motor\Revision\Http\Controllers\Backend\TravelersController;
+use Motor\Revision\Http\Controllers\Backend\ShuttlesController;
+use Motor\Revision\Http\Controllers\Backend\RidesController;
+use Motor\Revision\Http\Controllers\Backend\SponsorsController;
+use Motor\Revision\Http\Controllers\Backend\HotelsController;
+use Motor\Revision\Http\Controllers\Backend\Component\ComponentTicketsController;
+
 Route::group([
     'as'         => 'backend.',
     'prefix'     => 'backend',
-    'namespace'  => 'Motor\Revision\Http\Controllers\Backend',
     'middleware' => [
         'web',
         'web_auth',
         'navigation',
     ],
 ], static function () {
-    Route::resource('tickets', 'TicketsController');
-    Route::resource('airports', 'AirportsController');
-    Route::resource('travelers', 'TravelersController');
-    Route::resource('shuttles', 'ShuttlesController');
-    Route::resource('rides', 'RidesController');
-    Route::resource('sponsors', 'SponsorsController');
-    Route::resource('hotels', 'HotelsController');
+    Route::resource('tickets', TicketsController::class);
+    Route::resource('airports', AirportsController::class);
+    Route::resource('travelers', TravelersController::class);
+    Route::resource('shuttles', ShuttlesController::class);
+    Route::resource('rides', RidesController::class);
+    Route::resource('sponsors', SponsorsController::class);
+    Route::resource('hotels', HotelsController::class);
 });
 
 // Only add the route group if you don't already have one for the given namespace
 Route::group([
     'as'         => 'component.',
     'prefix'     => 'component',
-    'namespace'  => 'Motor\Revision\Http\Controllers\Backend\Component',
     'middleware' => [
         'web',
     ],
 ], function () {
     // You only need this part if you already have a component group for the given namespace
-    Route::get('tickets', 'ComponentTicketsController@create')
+    Route::get('tickets', [ComponentTicketsController::class, 'create'])
          ->name('tickets.create');
-    Route::post('tickets', 'ComponentTicketsController@store')
+    Route::post('tickets', [ComponentTicketsController::class, 'store'])
          ->name('tickets.store');
-    Route::get('tickets/{component_ticket}', 'ComponentTicketsController@edit')
+    Route::get('tickets/{component_ticket}', [ComponentTicketsController::class, 'edit'])
          ->name('tickets.edit');
-    Route::patch('tickets/{component_ticket}', 'ComponentTicketsController@update')
+    Route::patch('tickets/{component_ticket}', [ComponentTicketsController::class, 'update'])
          ->name('tickets.update');
 });
